4c94c05c574a2b10e39cb6f5c3e5cbaf3679444d
Daan Hage <daan@atabix.nl>
2022-02-03 11:58:17 +0100

    Update php-cs-fixer.config.php

--- php-cs-fixer.config.php
+++ php-cs-fixer.config.php
@@ -60,6 +60,6 @@ return $config
 		'phpdoc_trim'                                 => true,
 		'phpdoc_types'                                => true,
 		'phpdoc_var_without_name'                     => true,
-		'types_spaces'				      => ['space' => 'none']
+		'types_spaces'                                => ['space' => 'none']
 	])
 	->setFinder($finder);
